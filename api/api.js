97da6ba09bad33efb257bcd592dbfad18a8e7e2c
user2e23 <113674920+ElCreaturoM50S@users.noreply.github.com>
2024-02-22 19:51:29 +0100

    Mongo

--- api/api.js
+++ api/api.js
@@ -3,28 +3,28 @@ const orzech = require('../orzech/orzech.js');
 const router = express.Router();
 
 //działa
-router.get("/orzech", (req, res) => {
+router.get("/orzech", async (req, res) => {
     res.json(orzech.list());
 })
 
 //działa
-router.post("/orzech", (req,res)=> {
+router.post("/orzech", async (req,res)=> {
     res.json(orzech.add(req.body));
 })
 
 //działa
-router.put("/orzech/:id", (req,res)=>{
+router.put("/orzech/:id", async (req,res)=>{
     req.body.id = parseInt(req.params.id);
     req.json(orzech.update(req.body));
 })
 
 //działa
-router.get("/orzech/:id", (req, res)=>{
+router.get("/orzech/:id", async (req, res)=>{
     res.json(orzech.get(req.params.id));
 })
 
 //działa
-router.delete("/orzech/:id", (req,res)=>{
+router.delete("/orzech/:id", async (req,res)=>{
     res.json(orzech.delete(parseInt(req.params.id)))
 })
 
